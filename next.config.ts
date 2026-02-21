f4168181cb93885c0e465d5ce2f914b7a6c14d02
SeiRan <bo646ru@gmail.com>
2026-02-21 11:57:35 -0500

    Configuration for the .mdx format
    Signed-off-by: SeiRan <bo646ru@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import nextMdx from "@next/mdx";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withMdx = nextMdx({
+  extension: /\.mdx?$/,
+  options: {},
+});
+
+const nextConfig: NextConfig = withMdx({
+  pageExtensions: ["md", "mdx", "tsx", "ts", "jsx", "js"],
+});
 
 export default nextConfig;
