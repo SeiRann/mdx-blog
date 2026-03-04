31b65a9ebcf35c0e825a524531a5bd78a38672be
SeiRan <bo646ru@gmail.com>
2026-03-04 00:49:23 -0500

    hr color styling
    Signed-off-by: SeiRan <bo646ru@gmail.com>

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -52,7 +52,12 @@ export function useMDXComponents(): MDXComponents {
     ),
     em: (props) => <em className="italic" {...props} />,
     strong: (props) => <strong className="font-bold" {...props} />,
-    hr: (props) => <hr className="my-4" {...props} />,
+    hr: (props) => (
+      <hr
+        className="my-4 dark:text-dark-blue-foreground text-red-foreground"
+        {...props}
+      />
+    ),
     img: (props) => <img className="my-4 rounded-md" {...props} />,
     li: (props) => <li className="ml-4" {...props} />,
     ol: (props) => <ol className="my-2 ml-4 list-decimal" {...props} />,
