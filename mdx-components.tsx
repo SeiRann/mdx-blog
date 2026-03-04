977383202d4c16f9a404e86eac89df84d4d434c7
SeiRan <bo646ru@gmail.com>
2026-03-04 00:37:04 -0500

    text rwd
    Signed-off-by: SeiRan <bo646ru@gmail.com>

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,47 +3,47 @@ import Link from "next/link";
 
 const components: MDXComponents = {};
 
-export function useMDXComponents(): MDXComponents {
+export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: (props) => (
       <h1
-        className="text-6xl dark:text-dark-blue-foreground text-red-foreground"
+        className="text-2xl sm:text-3xl md:text-6xl dark:text-dark-blue-foreground text-red-foreground"
         {...props}
       />
     ),
     h2: (props) => (
       <h2
-        className="text-4xl dark:text-dark-blue-foreground text-red-foreground"
+        className="text-xl sm:text-2xl md:text-5xl   dark:text-dark-blue-foreground text-red-foreground"
         {...props}
       />
     ),
     h3: (props) => (
       <h3
-        className="text-2xl dark:text-dark-blue-foreground text-red-foreground"
+        className="text-lg sm:text-xl md:text-4xl dark:text-dark-blue-foreground text-red-foreground"
         {...props}
       />
     ),
     h4: (props) => (
       <h4
-        className="text-lg dark:text-dark-blue-foreground text-red-foreground"
+        className="text-md sm:text-lg md:text-3xl dark:text-dark-blue-foreground text-red-foreground"
         {...props}
       />
     ),
     h5: (props) => (
       <h5
-        className="text-md dark:text-dark-blue-foreground text-red-foreground"
+        className="text-sm sm:text-md md:text-xl dark:text-dark-blue-foreground text-red-foreground"
         {...props}
       />
     ),
     h6: (props) => (
       <h6
-        className="text-sm dark:text-dark-blue-foreground text-red-foreground"
+        className="text-xs sm:text-sm md:text-md dark:text-dark-blue-foreground text-red-foreground"
         {...props}
       />
     ),
     p: (props) => (
       <p
-        className="my-2 dark:text-dark-blue-foreground text-red-foreground"
+        className="text-sm sm:text-md md:text-lg  my-2 dark:text-dark-blue-foreground text-red-foreground"
         {...props}
       />
     ),
@@ -53,20 +53,30 @@ export function useMDXComponents(): MDXComponents {
     em: (props) => <em className="italic" {...props} />,
     strong: (props) => <strong className="font-bold" {...props} />,
     hr: (props) => <hr className="my-4" {...props} />,
-    img: (props) => <img className="my-4 rounded-md" {...props} />,
+    img: (props) => <img className=" rounded-md" {...props} />,
     li: (props) => <li className="ml-4" {...props} />,
     ol: (props) => <ol className="my-2 ml-4 list-decimal" {...props} />,
     ul: (props) => <ul className="my-2 ml-4 list-disc" {...props} />,
     blockquote: (props) => <blockquote className="" {...props} />,
-    code: (props) => (
-      <code
-        className="p-1 rounded-sm bg-background-3 dark:bg-dark-background-0h"
-        {...props}
-      />
-    ),
+    code: ({ className, ...props }) => {
+      const isInline = !className;
+
+      if (isInline) {
+        return (
+          <code
+            className="px-1 py-0.5 rounded bg-background-3 dark:bg-dark-background-0h"
+            {...props}
+          />
+        );
+      }
+
+      // For block code, let <pre> handle styling
+      return <code className={className} {...props} />;
+    },
+
     pre: (props) => (
       <pre
-        className="p-1 w-1/3 rounded-sm ml-2 bg-background-3 dark:bg-dark-background-0s my-4"
+        className="p-1 rounded-md bg-background-3 dark:bg-dark-background-0s my-4 overflow-x-auto"
         {...props}
       />
     ),
