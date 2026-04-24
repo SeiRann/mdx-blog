545e400e978f5ff6136343790de7280ee0d07808
SeiRan <bo646ru@gmail.com>
2026-04-24 16:40:56 -0400

    added clickable3d that can take any shape
    Signed-off-by: SeiRan <bo646ru@gmail.com>

--- app/threejs/page.tsx
+++ app/threejs/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import Cube from "@/components/cube";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import Clickable3D from "@/components/clickable3d";
 
 export default function Page() {
+  const { scene } = useGLTF("/housemodel.glb");
+
   return (
     <div className="w-screen h-screen">
       <Canvas camera={{ position: [2, 2, 10], fov: 50 }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} intensity={1} />
 
-        <Cube />
+        <Clickable3D scene={scene} />
       </Canvas>
     </div>
   );
--- components/clickable3d.tsx
+++ components/clickable3d.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
+import { ObjectMap, useFrame } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
+import { Object3D, Group } from "three";
 
-export default function Cube() {
+type Props = {
+  scene: Object3D;
+};
+
+export default function Clickable3D({ scene }: Props) {
   const group = useRef<any>(null);
   const hovered = useRef(false);
 
-  const { scene } = useGLTF("/housemodel.glb");
   console.log(scene); // debug if the model is loaded
 
   useFrame((state, delta) => {
