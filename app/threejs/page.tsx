9fbb4a830a8948ff9652cc97061be3b1afcb5bd6
SeiRan <bo646ru@gmail.com>
2026-04-24 16:43:48 -0400

    changed model to test new clickable
    Signed-off-by: SeiRan <bo646ru@gmail.com>

--- app/threejs/page.tsx
+++ app/threejs/page.tsx
@@ -4,11 +4,11 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 import Clickable3D from "@/components/clickable3d";
 
 export default function Page() {
-  const { scene } = useGLTF("/housemodel.glb");
+  const { scene } = useGLTF("/gtr.glb");
 
   return (
     <div className="w-screen h-screen">
-      <Canvas camera={{ position: [2, 2, 10], fov: 50 }}>
+      <Canvas camera={{ position: [250, 10, 0], fov: 80 }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} intensity={1} />
 
